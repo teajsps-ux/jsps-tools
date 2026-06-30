ef412c32eb62815e93ecc8b2d7e07a74a83d07bf
teajsps-ux <teajsps@gmail.com>
2026-06-30 10:04:53 +0800

    Fix Knowledge King local helper CORS for Pages

--- tools/knowledge-king-pk/local-quiz-csv-server.mjs
+++ tools/knowledge-king-pk/local-quiz-csv-server.mjs
@@ -22,7 +22,8 @@ function sendJson(res, status, payload) {
     "Content-Type": "application/json; charset=utf-8",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+    "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+    "Access-Control-Allow-Private-Network": "true"
   });
   res.end(JSON.stringify(payload));
 }
